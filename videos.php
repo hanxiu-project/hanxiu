c2355b7141ea4d027fade268e3ebcabe6306be38
BingYoyo <peeee0317@gmail.com>
2021-01-26 23:42:29 +0800

    新增影音專區前台後台

---
+++ videos.php
@@ -0,0 +1,172 @@
+<html>
+<head>
+
+    <title>test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="./csss_file/cssfornophoto3.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js"
+            integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+"
+            crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+<?php
+$db_ip = "127.0.0.1";
+$db_user = "root";
+$db_pwd = "123456789";
+$db_link = @mysqli_connect($db_ip, $db_user, $db_pwd, "專題");
+mysqli_query($db_link, 'SET CHARACTER SET utf8');
+//載入資料庫連線與啟用session
+//include("sql.php");
+session_start();
+
+?>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+
+
+<!--最外圍-->
+<div id="sitebody">
+
+
+    <!--頁首-->
+    <!--包住固定不動的Header-->
+    <div id="header2">
+
+        <div id="header">
+            <img src="logo3.jpg" align="left" width="auto" height="100">
+            <div id="wrapnav1">
+                <nav>
+                    <ul class="flex-nav ">
+                        <?php
+                        $sql = "SELECT * FROM posts";
+                        $result = mysqli_query($db_link, $sql);
+                        $sql_search_acc = "SELECT * FROM `members` WHERE `account` = '$_SESSION[acc]'";
+                        $resultsrchacc = mysqli_query($db_link, $sql_search_acc);
+                        $rows = mysqli_fetch_assoc($resultsrchacc);
+                        $acc = $rows["account"];
+                        $name = $rows["name"];
+
+                        if ($_SESSION[acc] == null) {
+                            echo "<li>";
+                            echo "<a href='login.php'>登入</a>";
+                            echo "</li>";
+                            echo "<li>";
+                            echo "<a href='registered.php'>註冊</a>";
+                            echo "</li>";
+                        } else if ($acc == $_SESSION['acc']) {
+
+                            echo "<li>";
+                            echo "<a href='#'><b>$name</b>，您好</a>";
+                            echo "</li>";
+                            echo "<li>";
+                            echo "<a href='logout.php' >登出</a>";
+                            echo "</li>";
+                        }
+                        ?>
+                    </ul>
+                </nav>
+
+            </div>
+           <div id="wrapnav2">
+                <nav>
+                    <ul class="flex-nav ">
+                        <li>
+                            <a href="https://www.facebook.com/">
+                                <i class="fab fa-facebook-square" aria-hidden="true" title="臉書粉絲專頁"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.gmail.com/">
+                                <i class="fas fa-envelope-square" aria-hidden="true" title="電子信箱"></i>
+                            </a>
+                        </li>
+                        <li><a href="indexs.php">回首頁</a></li>
+                        <li><a href="articletype.php">經文閱讀</a></li>
+                        <li><a href="news.php">最新公告</a></li>
+                        <li><a href="Memberdonates.php">查看捐獻</a></li>
+                        <li><a href="?">個人資料</a></li>
+                        <li><a href="?">留言區</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+            crossorigin="anonymous"></script>
+
+    <!--照片區-->
+
+
+    <!--左邊欄位
+    <div id="sidebar_left">sidebar_left</div>
+
+
+
+
+    右邊欄位
+    <div id="sidebar_right">sidebar_right</div>-->
+
+
+    <!--主內文區-->
+    <div id="content">
+        <div class="newstitle">
+            <div class="contentlist">
+                <?php
+$db_ip="127.0.0.1";
+                $db_user="root";
+                $db_pwd="123456789";
+                $db_link=@mysqli_connect($db_ip, $db_user, $db_pwd, "專題");
+                mysqli_query($db_link, 'SET CHARACTER SET utf8');
+                $result= mysqli_query($db_link,$sql);
+				$sql = "SELECT * FROM videos";
+                $result= mysqli_query($db_link,$sql);
+
+                ?>
+				<h3>|電音專區</h3>
+				<div class="table" align="center">
+                
+                <?php
+                echo "<form name='form1' method='POST' action=''>";
+                echo "<table  width=1600 style=font-size:24px; >";
+                echo "<tr align=center>";
+               
+                echo "<td></td>";
+                echo "</tr>";
+                while($row=$result->fetch_assoc())
+                {
+                    echo "<tr align=center>";
+                    
+                    echo "<td width=70%><left><iframe width=300 height=200 src=$row[vnet] frameborder=0 allow=accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen></iframe></td>";
+					echo "<td width=30%>$row[vcontent]</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                ?>
+			
+			</div>
+            </div>
+
+        </div>
+
+
+    </div>
+    
+    <!--註腳-->
+    <footer class="footer">版權所有 © 勤益科大</footer>
+
+
+</div>
+
+</body>
+
+
+</html>

241a258f11f0e94ec6f727f177950c6ccfc54f53
DESKTOP-BSJBQSM\USER <2365275a@gmail.com>
2021-03-01 00:14:07 +0800

    幻燈片

--- login.php
+++ login.php
@@ -129,14 +129,14 @@
                         <img src="圖6.jpg" class="d-block w-100" height="500px" alt="...">
                     </div>
                 </div>
-				 <!--<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
-                </a>-->
+                </a>
 			</div>
 				
 				</div>

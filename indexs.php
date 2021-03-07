6f29320af4008f759c4d93279d136a28459566ca
DESKTOP-BSJBQSM\USER <2365275a@gmail.com>
2021-03-07 21:58:58 +0800

    333

--- indexs.php
+++ indexs.php
@@ -59,12 +59,8 @@
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
 
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                   
+                    
 
                     <?php
                         $sql1 = "SELECT images FROM carousel";

394e147b2648f257a1df7326d276c2d77280596c
Xu jia quan <quannn0180716v@gmail.com>
2021-03-07 21:55:27 +0800

    header登入顯示名稱問題

--- indexs.php
+++ indexs.php
@@ -59,13 +59,6 @@
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
 
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-
                     <?php
                         $sql1 = "SELECT images FROM carousel";
                         $result1 = mysqli_query($db_link,$sql1);

6cc118a4456f53378d988ec640f27279aa2a80de
Xu jia quan <quannn0180716v@gmail.com>
2021-07-04 15:41:48 +0800

    修改寄送驗證碼網址

--- registered.php
+++ registered.php
@@ -212,7 +212,7 @@
                         $email->Password = '3a732087ncut';
                         $email->setFrom('xuj8906@gmail.com',"漢修學院");
                         $email->Subject = 'Email註冊驗證信通知';
-                        $email->Body = "<a href='http://localhost/hanxiu/verify.php?vkey=$vkey'>註冊帳號</a>";
+                        $email->Body = "<a href='http://140.128.80.207/hanxiu/verify.php?vkey=$vkey'>註冊帳號</a>";
                         $email->addAddress("$e");
                         $email->SMTPDebug = 0;
 
